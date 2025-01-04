a86581b3343d905a35c52b15bb31edb882be6a15
JasonKok-2006 <jkok1726@gmail.com>
2025-01-04 15:06:35 +0000

    grid made

--- 2048.py
+++ 2048.py
@@ -3,19 +3,43 @@ import sys
 
 pygame.init()
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+#screen parameters
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+
+tile_size = 100
+
+#colours
+bg = (200, 200, 200)
+line = (0, 0, 0)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+#This finction draws the grid to keep the tiles in
+def draw_grid(tile_size):
+    screen.fill(bg)
+
+    #veritcal lines
+    for x in range(tile_size, SCREEN_WIDTH, tile_size):
+        pygame.draw.line(screen, line, (x,0), (x, SCREEN_HEIGHT))
+
+    #horizontal lines
+    for x in range(tile_size, SCREEN_HEIGHT, tile_size):
+        pygame.draw.line(screen, line, (0, x), (SCREEN_WIDTH, x))
+
+
 run = True
 while run:
-
     #check if the close button has been pressed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             run == False
+
+    draw_grid(tile_size)
+
+    pygame.display.update()
+
 
 pygame.display.quit()
 pygame.quit()
